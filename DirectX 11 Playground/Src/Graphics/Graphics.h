ea10993ceee95d9b777de08d5e000701c5f0374e
joseph kalathil <josephbk.117@hotmail.com>
2019-05-04 20:59:21 +0530

    set up assimp for loading models

--- DirectX 11 Playground/Src/Graphics/Graphics.h
+++ DirectX 11 Playground/Src/Graphics/Graphics.h
@@ -1,8 +1,6 @@
 #pragma once
 #include <d3d11.h>
 #include <wrl\client.h>
-#pragma comment(lib, "d3d11.lib")
-#pragma comment(lib, "DirectXTK.lib")
 #include <WICTextureLoader.h>
 #include "AdapterReader.h"
 #include "Shader.h"
